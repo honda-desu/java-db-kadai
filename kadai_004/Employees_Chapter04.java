05718074ec3888caff7bcbbe8235af9bf95b4dfd
honda-desu <h9.k.honda@gmail.com>
2025-04-09 09:45:19 +0900

    Update Employees_Chapter04.java

--- kadai_004/Employees_Chapter04.java
+++ kadai_004/Employees_Chapter04.java
@@ -16,7 +16,7 @@ public class Employees_Chapter04 {
 			con = DriverManager.getConnection(
 					"jdbc:mysql://localhost/challenge_java",
 					"root",
-					""
+					"password"
 			);
 			
 			System.out.println("データベース接続成功");
